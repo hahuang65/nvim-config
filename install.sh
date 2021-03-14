0fdb51513c2605d54ff08d6ae42c1767dc4fe2d7
Howard Huang <h.huang65@gmail.com>
2021-03-13 21:22:53 -0800

    FIX: Prevent lua/ from nesting
    Using `ln -sf` with the `lua` directory would keep nesting a recursive
    link everytime `./install.sh` is run. Instead, using `--no-dereference`
    prevents it from happening.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#/bin/sh
 
 mkdir -p "${HOME}/.config/nvim"
 
 ln -sf "${PWD}/init.vim" "${HOME}/.config/nvim/init.vim"
-ln -sf "${PWD}/lua" "${HOME}/.config/nvim/lua"
+ln -snf "${PWD}/lua" "${HOME}/.config/nvim/lua"
