13d683149cd036d9e30e82c85f19169a9241911a
Howard Huang <h.huang65@gmail.com>
2021-03-18 13:35:13 -0700

    Enable rust_analyzer for LSP

--- lua/plugin/lsp.lua
+++ lua/plugin/lsp.lua
@@ -6,12 +6,14 @@
 -- npm install -g dockerfile-language-server-nodejs
 -- brew install hashicorp/tap/terraform-ls / paru -S terraform-ls
 -- pip install 'python-language-server[all]' (run `asdf reshim python` to get the executable accessible)
+-- paru -S rust-analyzer
 -- npm install -g vls
 -- npm install -g vim-language-server
 local servers = {
   'bashls',
   'dockerls',
   'pyls',
+  'rust_analyzer',
   'solargraph',
   'terraformls',
   'vimls',
