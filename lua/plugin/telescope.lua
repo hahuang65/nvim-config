b23249f624f6a9a0d12f7eaad095b16e35589bda
Howard Huang <h.huang65@gmail.com>
2021-03-26 14:46:44 -0700

    Re-order keybinds for telescope, add picker for help modules

--- lua/plugin/telescope.lua
+++ lua/plugin/telescope.lua
@@ -32,15 +32,16 @@ find_project_files = function(opts)
 end
 
 vim.api.nvim_set_keymap('n', '<leader><leader>', [[<cmd>lua find_project_files()<CR>]], { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>b', [[<cmd>Telescope buffers<CR>]], { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>/', [[<cmd>Telescope live_grep<CR>]], { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>f', [[<cmd>Telescope find_files<CR>]], { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>b', [[<cmd>Telescope buffers<CR>]], { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>c', [[<cmd>lua find_vim_config()<CR>]], { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>d', [[<cmd>lua find_dotfiles()<CR>]], { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>f', [[<cmd>Telescope find_files<CR>]], { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>gg', [[<cmd>Telescope git_status<CR>]], { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>gb', [[<cmd>Telescope git_branches<CR>]], { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>gc', [[<cmd>Telescope git_bcommits<CR>]], { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>gC', [[<cmd>Telescope git_commits<CR>]], { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>h', [[<cmd>Telescope help_tags<CR>]], { noremap = true })
 
 -- telescope-project
 vim.api.nvim_set_keymap('n', '<leader>pp', ":lua require'telescope'.extensions.project.project{}<CR>", {noremap = true, silent = true})
