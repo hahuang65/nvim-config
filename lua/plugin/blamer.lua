4e32d495c3debe8e28ada8bc05ddb32241028159
Howard Huang <h.huang65@gmail.com>
2021-04-13 23:06:18 -0700

    Change blamer section separator

--- lua/plugin/blamer.lua
+++ lua/plugin/blamer.lua
@@ -3,7 +3,7 @@
 require'nvim-blamer'.setup({
     enable = true,
     prefix = '    ',
-    format = '%committer │ %committer-time-human │ %summary',
+    format = '%committer • %committer-time-human • %summary',
 })
 
 vim.fn['nvimblamer#auto']()
