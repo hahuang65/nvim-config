afc635af7cc4598e46d575d6e4b66c6dc1086b26
Howard Huang <h.huang65@gmail.com>
2021-07-06 15:45:14 -0700

    Add keybinds for PR, and git conflict resolution

--- lua/plugin/fugitive.lua
+++ lua/plugin/fugitive.lua
@@ -1,10 +1,15 @@
 -- https://github.com/tpope/vim-fugitive
+-- https://github.com/pwntester/octo.nvim
 
 local map = vim.api.nvim_set_keymap
 
-map('n', '<leader>gA', ':Git amend<CR>',                       { noremap = true })
-map('n', '<leader>gg', ':Git<CR>',                             { noremap = true })
-map('n', '<leader>gP', ':Git publish<CR>',                     { noremap = true })
-map('n', '<leader>gR', ':Git retrunk<CR>',                     { noremap = true })
-map('n', '<leader>gS', ':Git sync<CR>',                        { noremap = true })
-map('n', '<leader>pr', ':Git pr create --fill --web<CR>',      { noremap = true })
+-- Conflicts
+map('n', 'd[', ':diffget //2 | :diffupdate<CR>',       { noremap = true })
+map('n', 'd]', ':diffget //3 | :diffupdate<CR>',       { noremap = true })
+
+map('n', '<leader>gA',  ':Git amend<CR>',         { noremap = true })
+map('n', '<leader>gg',  ':Git<CR>',               { noremap = true })
+map('n', '<leader>gP',  ':Git publish<CR>',       { noremap = true })
+map('n', '<leader>gR',  ':Git retrunk<CR>',       { noremap = true })
+map('n', '<leader>gS',  ':Git sync<CR>',          { noremap = true })
+map('n', '<leader>pr',  ':Octo review start<CR>', { noremap = true })
