50d79b1b60c5ef22ad7a6c00ae91303636878c61
Howard Huang <h.huang65@gmail.com>
2021-07-23 14:16:54 -0700

    Change instructions on how to install pylsp

--- lua/plugin/lsp.lua
+++ lua/plugin/lsp.lua
@@ -4,7 +4,7 @@
 -- npm install -g bash-language-server
 -- npm install -g dockerfile-language-server-nodejs
 -- brew install hashicorp/tap/terraform-ls / paru -S terraform-ls
--- pip install 'python-language-server[all]' (run `asdf reshim python` to get the executable accessible)
+-- pip install 'python-lsp-server' (run `asdf reshim python` to get the executable accessible)
 -- paru -S rust-analyzer / brew install rust-analyzer
 -- npm install -g typescript-language-server
 -- npm install -g vls
