fea2b8343b410446420d928d90d8f79cee4d78db
Howard Huang <h.huang65@gmail.com>
2021-10-04 16:22:44 -0700

    Update nvim-tree settings

--- lua/plugin/nvim-tree.lua
+++ lua/plugin/nvim-tree.lua
@@ -1,8 +1,16 @@
 -- https://github.com/kyazdani42/nvim-tree.lua
 
+require'nvim-tree'.setup {
+  disable_netrw = true,
+  auto_close = true,
+  update_cwd = true,
+  update_focused_file = {
+    enable = true
+  }
+}
+
 vim.g.nvim_tree_ignore = { '.git', 'node_modules' } -- Ignore certain subdirectories/files
 vim.g.nvim_tree_indent_markers = 1                  -- Show indentation markers for subdirectories
-vim.g.nvim_tree_follow = 1                          -- Updates tree cursor when a buffer is entered
 vim.g.nvim_tree_add_trailing = 1                    -- Adds a trailing `/` to denote directories
 vim.g.nvim_tree_group_empty = 1                     -- If a directory has a single child, combine into 1 entry
-vim.g.nvim_tree_disable_netrw = 0                   -- Don't disable netrw. It has useful facilities, such as `gx`
+vim.g.nvim_tree_quit_on_open = 1                    -- Closes tree when opening a file
