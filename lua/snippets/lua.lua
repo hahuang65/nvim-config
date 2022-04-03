df2c4f8fbe561a1b84c9fd8ffff6ab618461b71d
Howard Huang <hao@hwrd.me>
2022-04-02 21:30:18 -0700

    Add some snippets for lua
    A snippet that defines a function, optionally as a local function.
    A snippet that can define an if statement, with options to include else
    and elseif statements. The latter can be added infinitely using
    recursion.

--- lua/snippets/lua.lua
+++ lua/snippets/lua.lua
@@ -1,8 +1,40 @@
 local ls = require"luasnip"
+local c = ls.choice_node
+local d = ls.dynamic_node
 local f = ls.function_node
 local i = ls.insert_node
 local s = ls.s
+local sn = ls.snippet_node
+local t = ls.text_node
 local fmt = require("luasnip.extras.fmt").fmt
+
+local function endif()
+  -- This function represents a snippet choice for
+  -- 1) `end`ing
+  -- 2) `else...end`ing
+  -- 3) `elseif...then...end`ing
+  -- an `if statement. If using the `elseif`, the `end` can recursively call itself
+  -- such that another `elseif...then...end` or `else...end` can be used.
+  return sn(nil, {
+    c(1, {
+      t("end"),
+      sn(nil, {
+        t("else"),
+        t({"", "\t"}),
+        i(1),
+        t({"", "end"})
+      }),
+      sn(nil, {
+        t("elseif "),
+        i(1),
+        t({" then", "\t"}),
+        i(2),
+        t({"", ""}),
+        d(3, endif, {})
+      })
+    })
+  })
+end
 
 return {
   s("req",
@@ -10,5 +42,39 @@ return {
       local parts = vim.split(import_name[1][1], ".", true)
       return parts[#parts] or ""
     end, { 1 }), i(1) })
+  ),
+
+  s("func",
+    fmt(
+      [[
+      {} {}({})
+        {}
+      end
+      ]],
+      {
+        c(1, {
+          t("function"),
+          t("local function")
+        }),
+        i(2, "name"),
+        i(3),
+        i(0)
+      }
+    )
+  ),
+
+  s("if",
+    fmt(
+      [[
+        if {} then
+          {}
+        {}
+      ]],
+      {
+        i(1),
+        i(2),
+        d(3, endif, {})
+      }
+    )
   )
 }
