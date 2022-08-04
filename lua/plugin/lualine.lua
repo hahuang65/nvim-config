70ef5f83a6f8bce0ac7021f5d2d03a069530873f
Howard Huang <hao@hwrd.me>
2022-08-03 17:33:58 -0700

    Fix lualine theme not working properly
    Going back to the manual configuration for now.

--- lua/plugin/lualine.lua
+++ lua/plugin/lualine.lua
@@ -51,7 +51,13 @@ local config = {
     -- Disable sections and component separators
     component_separators = "",
     section_separators = "",
-    theme = "catppuccin"
+    theme = {
+      -- We are going to use lualine_c an lualine_x as left and
+      -- right section. Both are highlighted by c theme .  So we
+      -- are just setting default looks o statusline
+      normal = {c = {fg = Colors.fg, bg = Colors.bg}},
+      inactive = {c = {fg = Colors.fg, bg = Colors.bg}}
+    }
   },
   sections = {
     -- these are to remove the defaults
@@ -165,8 +171,8 @@ ins_left {
       t = Colors.red
     }
     vim.api.nvim_command(
-        'hi! LualineMode guifg=' .. mode_color[vim.fn.mode()] .. " guibg=" ..
-            Colors.bg)
+      'hi! LualineMode guifg=' .. mode_color[vim.fn.mode()] .. " guibg=" .. Colors.bg
+    )
     return ''
   end,
   color = "LualineMode"
