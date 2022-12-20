08a28d1a5c6503c418b728914031c25b2b308342
Howard Huang <hao@hwrd.me>
2022-12-20 15:28:43 -0800

    Add keymap help

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 -- https://github.com/nvim-telescope/telescope.nvim
 
 local actions = require('telescope.actions')
-require('telescope').setup{
+require('telescope').setup {
   defaults = {
     mappings = {
       n = {
@@ -17,13 +17,15 @@ require('telescope').setup{
 -- Enable telescope fzf native, if installed
 pcall(require('telescope').load_extension, 'fzf')
 
-vim.keymap.set('n', '<leader><leader>', require("finders").project_files,                       { desc = 'Search Project/Files' })
-vim.keymap.set('n', '<leader>sb',       require('telescope.builtin').buffers,                   { desc = '[S]earch [B]uffers' })
-vim.keymap.set('n', '<leader>sf',       require('telescope.builtin').find_files,                { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sh',       require('telescope.builtin').help_tags,                 { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>*',        require('telescope.builtin').grep_string,               { desc = 'Search current word' })
-vim.keymap.set('n', '<leader>/',        require('telescope.builtin').current_buffer_fuzzy_find, { desc = 'Search Project via grep' })
-vim.keymap.set('n', '<leader>?',        require('telescope.builtin').live_grep,                 { desc = 'Search current file' })
+vim.keymap.set('n', '<leader><leader>', require("finders").project_files, { desc = 'Search Project/Files' })
+vim.keymap.set('n', '<leader>sb', require('telescope.builtin').buffers, { desc = '[S]earch [B]uffers' })
+vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+vim.keymap.set('n', '<leader>sk', require('telescope.builtin').keymaps, { desc = '[S]earch [K]eymaps' })
+vim.keymap.set('n', '<leader>*', require('telescope.builtin').grep_string, { desc = 'Search current word' })
+vim.keymap.set('n', '<leader>/', require('telescope.builtin').current_buffer_fuzzy_find,
+  { desc = 'Search Project via grep' })
+vim.keymap.set('n', '<leader>?', require('telescope.builtin').live_grep, { desc = 'Search current file' })
 
 vim.keymap.set('n', '<leader>sd', function()
   require('telescope.builtin').diagnostics({ bufnr = true })
