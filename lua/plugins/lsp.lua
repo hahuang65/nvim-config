cbc2d3d8249c51c493d3f82ca99d3a9ce9e8ee44
Howard Huang <hao@hwrd.me>
2023-02-22 11:53:30 -0800

    Disable diagnostics during insert

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -51,7 +51,7 @@ return {
     }
 
     vim.diagnostic.config({
-      update_in_insert = true,
+      update_in_insert = false,
       signs = false,
       severity_sort = true,
       virtual_text = {
