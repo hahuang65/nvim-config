d5cf3a3c025c49569400c8450f802c29f62321be
Howard Huang <hao@hwrd.me>
2023-05-11 08:15:09 -0700

    Temporary fix for Telescope landing in insert mode
    https://github.com/nvim-telescope/telescope.nvim/issues/2027#issuecomment-1510001730

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -87,5 +87,14 @@ return {
 
     -- Enable telescope fzf native, if installed
     pcall(require("telescope").load_extension, "fzf")
+
+    -- Temporay workaround for bug: https://github.com/nvim-telescope/telescope.nvim/issues/2027#issuecomment-1510001730
+    vim.api.nvim_create_autocmd("WinLeave", {
+      callback = function()
+        if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
+          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "i", false)
+        end
+      end,
+    })
   end,
 }
