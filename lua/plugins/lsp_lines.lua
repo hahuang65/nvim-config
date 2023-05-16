dd6c6328954e9c719d0f6979b836a38cbe176840
Howard Huang <hao@hwrd.me>
2023-05-16 10:52:12 -0500

    Fix lsp_lines displaying in lazy.nvim
    https://github.com/folke/lazy.nvim/issues/620

--- lua/plugins/lsp_lines.lua
+++ lua/plugins/lsp_lines.lua
@@ -4,5 +4,8 @@ return {
   "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
   config = function()
     require("lsp_lines").setup()
+
+    -- https://github.com/folke/lazy.nvim/issues/620
+    vim.diagnostic.config({ virtual_lines = false }, require("lazy.core.config").ns)
   end,
 }
