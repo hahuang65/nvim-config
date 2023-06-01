4238207702f9ecf3722f1cf60d43c50b7d40e53f
Howard Huang <hao@hwrd.me>
2023-06-01 13:43:00 -0700

    Remove vim-rooter for nvim-rooter
    vim-rooter was causing Fugitive windows to autoclose (such as after
    `:Git sync`)

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,6 @@
 -- This file includes any plugins that don't require any setup or dependencies.
 -- Any other plugins will have their own file in lua/plugins/
 return {
-  "airblade/vim-rooter",
   "tpope/vim-repeat",
   "tpope/vim-surround",
   "romainl/vim-cool",
---
+++ lua/plugins/rooter.lua
@@ -0,0 +1,6 @@
+-- https://github.com/notjedi/nvim-rooter.lua
+--
+return {
+  "notjedi/nvim-rooter.lua",
+  config = {},
+}
