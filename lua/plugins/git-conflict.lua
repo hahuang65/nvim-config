3f04bf3d27b113ce7ba8d59d9ddd851dac413e10
Howard Huang <hao@hwrd.me>
2023-07-13 09:58:30 -0700

    Add git-conflict and pqf

---
+++ lua/plugins/git-conflict.lua
@@ -0,0 +1,16 @@
+-- https://github.com/akinsho/git-conflict.nvim
+return {
+  "akinsho/git-conflict.nvim",
+  version = "*",
+  config = function()
+    require("git-conflict").setup()
+
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "GitConflictDetected",
+      callback = function()
+        vim.notify("Conflict detected in " .. vim.fn.expand("<afile>"))
+        vim.cmd("GitConflictListQf")
+      end,
+    })
+  end,
+}
---
+++ lua/plugins/pqf.lua
@@ -0,0 +1,7 @@
+-- https://github.com/yorickpeterse/nvim-pqf
+return {
+  "yorickpeterse/nvim-pqf",
+  config = function()
+    require("pqf").setup()
+  end,
+}
