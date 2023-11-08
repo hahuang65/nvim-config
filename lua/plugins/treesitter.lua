b4729959e8370b2d8691d947aeb68db0e8fbd480
Howard Huang <hao@hwrd.me>
2023-11-08 10:01:08 -0600

    Add cooklang TS grammar, more Makefile snippets

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -14,6 +14,7 @@ return {
     require("nvim-treesitter.configs").setup({
       ensure_installed = {
         "bash",
+        "cooklang",
         "css",
         "dockerfile",
         "git_config",
