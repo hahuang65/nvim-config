3b8ba8dc67fdf52fee14b8f46ba7b87d46dd7f72
Howard Huang <hao@hwrd.me>
2023-12-18 12:44:05 -0600

    Add a dirty_check for Makefile snippets, remove golang
    As I should be using `cookiecutter` to generate my Golang projects

--- lua/snippets/make.lua
+++ lua/snippets/make.lua
@@ -16,7 +16,15 @@ local help = t({
 local confirm = t({
   ".PHONY: confirm",
   "confirm:",
-  "\t@echo -n 'Are you sure? [y/N] ' && read ans && [ $${ans:-N} = y ]",
+  "\t@echo 'Are you sure? [y/N] \\c' && read ans && [ $${ans:-N} = y ]",
+  "",
+  "",
+})
+
+local dirty = t({
+  ".PHONY: check_dirty",
+  "check_dirty:",
+  "\t	@git diff --quiet || $(error ERROR: Git workspace is dirty)",
   "",
   "",
 })
@@ -28,49 +36,8 @@ return {
   s("confirm", {
     confirm,
   }),
-  s("golang", {
-    help,
-    confirm,
-    t({
-      "## setup: bootstrap all dependencies for the project",
-      ".PHONY: setup",
-      "setup:",
-      "\tgo install honnef.co/go/tools/cmd/staticcheck@latest",
-      "\tgo install github.com/golangci/golangci-lint/cmd/golangci-lint@latest",
-      "\tgo install gotest.tools/gotestsum@latest",
-      "",
-      "## audit: tidy dependencies and format, vet, and test all code",
-      ".PHONY: audit",
-      "audit: format lint test",
-      "",
-      "## format: format codebase using `go fmt`",
-      ".PHONY: format",
-      "format:",
-      "\t@echo 'Formatting code...'",
-      "\tgo fmt ./...",
-      "",
-      "## lint: lint and vet codebase using `go vet`, `staticcheck` and `golangci-lint`",
-      ".PHONY: lint",
-      "lint:",
-      "\t@echo 'Linting and vetting code...'",
-      "\tgo vet ./...",
-      "\tstaticcheck ./...",
-      "\tgolangci-lint run",
-      "",
-      "## test: test codebase using `gotestsum`",
-      ".PHONY: test",
-      "test: deps",
-      "\t@echo 'Running tests...'",
-      "\tgo clean -testcache",
-      "\tgotestsum --format dots-v2 -- -race -vet=off ./...",
-      "",
-      "## deps: sync dependencies using `go mod tidy` and `go mod verify`",
-      ".PHONY: deps",
-      "deps:",
-      "\t@echo 'Tidying and verifying module dependencies...'",
-      "\tgo mod tidy",
-      "\tgo mod verify",
-    }),
+  s("dirty", {
+    dirty,
   }),
 }, {
   s("phony", {
