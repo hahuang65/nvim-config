ecb71675229b1b85516e8d3694142f543ad88272
Howard Huang <hao@hwrd.me>
2024-01-08 20:51:17 -0600

    Fix neovide fonts not resolving

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -1,6 +1,6 @@
 if vim.g.neovide then
   vim.opt.guifont =
-    "Iosevka,Noto Color Emoji, Symbols Nerd Font Mono, Powerline Extra Symbols, codicon, Noto Sans Symbols, Noto Sans Symbols2, Font Awesome 6 Free:h13"
+    "Iosevka,Noto_Color_Emoji,Symbols_Nerd_Font_Mono,Powerline_Extra_Symbols,codicon,Noto_Sans_Symbols,Noto_Sans_Symbols2,Font_Awesome_6_Free:h12"
   vim.g.neovide_input_macos_alt_is_meta = true
   vim.g.neovide_input_ime = false
 end
