37ca6ac9e54cfe7528a5d50f4ace00ba2d92f6cb
Howard Huang <hao@hwrd.me>
2024-01-09 21:36:23 -0600

    Add paste for neovide terminal panes

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -9,10 +9,17 @@ if vim.g.neovide then
     vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
     vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
     vim.keymap.set("i", "<D-v>", "<C-R>+") -- Paste insert mode
+    vim.keymap.set("t", "<D-v>", "<C-R>+") -- Paste terminal mode
+    vim.keymap.set("t", "<D-v>", function() -- Paste terminal mode
+      return '<C-\\><C-N>"+pi'
+    end, { expr = true })
   else
     vim.keymap.set("n", "<C-S-v>", '"+P') -- Paste normal mode
     vim.keymap.set("v", "<C-S-v>", '"+P') -- Paste visual mode
     vim.keymap.set("c", "<C-S-v>", "<C-R>+") -- Paste command mode
     vim.keymap.set("i", "<C-S-v>", "<C-R>+") -- Paste insert mode
+    vim.keymap.set("t", "<C-S-v>", function() -- Paste terminal mode
+      return '<C-\\><C-N>"+pi'
+    end, { expr = true })
   end
 end
