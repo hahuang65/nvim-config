0317ef934f6e16ef439e8664f207fb463c6ae9f9
Howard Huang <hao@hwrd.me>
2024-02-15 21:38:28 -0600

    Always load treesitter, so that grammars can be auto installed

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,6 @@
 
 return {
   "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
   dependencies = {
     "nvim-treesitter/nvim-treesitter-context",
     "nvim-treesitter/nvim-treesitter-textobjects",
@@ -15,6 +14,7 @@ return {
     require("nvim-treesitter.configs").setup({
       ensure_installed = {
         "bash",
+        "coffeescript",
         "cooklang",
         "css",
         "dockerfile",
