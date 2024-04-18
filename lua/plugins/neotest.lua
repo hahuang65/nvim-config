4c72e79a1c14a897af68da47effbe6d0190e98a7
Howard Huang <hao@hwrd.me>
2024-04-18 09:14:02 -0500

    Add nvim-nio dependency for neotest

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -3,6 +3,7 @@
 return {
   "nvim-neotest/neotest",
   dependencies = {
+    "nvim-neotest/nvim-nio",
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
     "antoinemadec/FixCursorHold.nvim",
