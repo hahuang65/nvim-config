4c87a3d4a509cf55a95d46563fd10935c7abb422
Howard Huang <hao@hwrd.me>
2024-05-13 12:46:24 -0500

    Make sourcing a file more obvious

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,1 +1,6 @@
-vim.keymap.set("n", "<leader>X", "<cmd>source %<CR>", { buffer = 0, desc = "Source current file" })
+function source_file()
+  vim.cmd("source %")
+  vim.notify("Loaded " .. vim.fn.expand("%"))
+end
+
+vim.keymap.set("n", "<leader>X", source_file, { buffer = 0, desc = "Source current file" })
