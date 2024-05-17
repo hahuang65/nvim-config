c0332b5563fd918cb1c7e7df2895524dae46cd18
Howard Huang <hao@hwrd.me>
2024-05-17 13:21:35 -0500

    Improve UX of oil.nvim

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,6 +1,21 @@
 -- https://github.com/stevearc/oil.nvim
 return {
   "stevearc/oil.nvim",
-  opts = {},
   dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("oil").setup({
+      columns = { "icon" },
+      view_options = {
+        show_hidden = true,
+      },
+      keymaps = {
+        ["<C-v>"] = "actions.select_vsplit",
+        ["<C-x>"] = "actions.select_split",
+        ["<C-s>"] = false,
+        ["<C-h>"] = false,
+      },
+    })
+
+    vim.keymap.set("n", "-", "<cmd>Oil<CR>", { desc = "Open parent directory as buffer" })
+  end,
 }
