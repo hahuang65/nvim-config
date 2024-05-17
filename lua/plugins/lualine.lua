45d68630584d7e9f5b1f8970f5eb35a32b1b263f
Howard Huang <hao@hwrd.me>
2024-05-16 21:15:56 -0500

    Fix deprecated function call

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -239,7 +239,7 @@ return {
       -- Lsp server name .
       function()
         local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-        local clients = vim.lsp.get_active_clients()
+        local clients = vim.lsp.get_clients()
         if next(clients) == nil then
           return ""
         end
