d628e2cc03dd517f153921f4e85cf55981f191d2
Howard Huang <hao@hwrd.me>
2024-12-23 11:51:18 -0600

    Remove glance

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -33,10 +33,10 @@ return {
       map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
 
       map("ge", vim.lsp.buf.declaration, "[G]oto d[E]claration")
-      map("gD", vim.lsp.buf.definition, "[G]oto [D]efinition")
-      map("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
-      map("gR", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-      map("gY", vim.lsp.buf.type_definition, "[G]oto t[Y]pe definition")
+      map("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+      map("gi", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+      map("gr", vim.lsp.buf.references, "[G]oto [R]eferences")
+      map("gy", vim.lsp.buf.type_definition, "[G]oto t[Y]pe definition")
       map("g]", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
       map("g}", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
 
