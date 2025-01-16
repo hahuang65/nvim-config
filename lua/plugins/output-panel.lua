e0b9e7407baeaddace2c2568b86f2ee01ede2057
Howard Huang <hao@hwrd.me>
2025-01-16 10:41:15 -0600

    Fix output-panel error

--- lua/plugins/output-panel.lua
+++ lua/plugins/output-panel.lua
@@ -2,9 +2,18 @@
 
 return {
   "mhanberg/output-panel.nvim",
-  cmd = "OutputPanel",
-  event = "LspAttach",
   config = function()
-    require("output_panel").setup()
+    require("output_panel").setup({
+      max_buffer_size = 5000, -- default
+    })
   end,
+  cmd = { "OutputPanel" },
+  keys = {
+    {
+      "<leader>o",
+      vim.cmd.OutputPanel,
+      mode = "n",
+      desc = "Toggle the output panel",
+    },
+  },
 }
