68c890f46f5f90efcb9ed3dcf9c9c3d586dbb80b
Howard Huang <hao@hwrd.me>
2025-02-24 14:44:25 -0600

    Update Claude model

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -9,7 +9,7 @@ return {
     auto_suggestions_provider = "claude", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
     claude = {
       endpoint = "https://api.anthropic.com",
-      model = "claude-3-5-sonnet-20241022",
+      model = "claude-3-7-sonnet-latest",
       temperature = 0,
       max_tokens = 4096,
     },
