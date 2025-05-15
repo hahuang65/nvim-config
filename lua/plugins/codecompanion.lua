8aa3e6d94bc918788fb0f50dda9cc71ca3e8005d
Howard Huang <hao@hwrd.me>
2025-05-14 22:57:39 -0500

    Change some CodeCompanion settings

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -40,9 +40,9 @@ return {
             ---On exiting and entering neovim, loads the last chat on opening chat
             continue_last_chat = true,
             ---When chat is cleared with `gx` delete the chat from history
-            delete_on_clearing_chat = false,
+            delete_on_clearing_chat = true,
             -- Picker interface ("telescope" or "snacks" or "default")
-            picker = "default",
+            picker = "snacks",
             ---Enable detailed logging for history extension
             enable_logging = false,
             ---Directory path to save the chats
