0da4fdbe6b6c0d12236d4ce077efa4af19629823
Howard Huang <hao@hwrd.me>
2025-05-22 12:42:13 -0500

    Update LuaSnip config

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -6,6 +6,7 @@
 return {
   "L3MON4D3/LuaSnip",
   event = "InsertEnter",
+  tag = "v2.*",
   build = "make install_jsregexp",
   keys = {
     {
@@ -55,16 +56,22 @@ return {
       require("snippets").edit_with_prompt,
       desc = "[S]nippets - [E]dit prompt for filetype",
     },
+
+    {
+      "<leader>sl",
+      function()
+        require("luasnip.extras.snippet_list").open()
+      end,
+      desc = "[S]nippets - [L]ist available snippets for buffer",
+    },
   },
   config = function()
     local ls = require("luasnip")
     local types = require("luasnip.util.types")
 
-    ls.config.set_config({
-      history = true, -- Allows you to jump back into the last snippet, even if you move outside it
-      update_events = "TextChanged,TextChangedI", -- Updates snippets as you type
-      delete_check_events = "TextChanged",
+    ls.setup({
       enable_autosnippets = true,
+
       ext_opts = {
         [types.choiceNode] = {
           active = {
@@ -77,6 +84,17 @@ return {
           },
         },
       },
+
+      -- New version of `history`, which allows jumping back into snippets after leaving
+      --  if (history) lot nil, keep_roots, link_roots, and link_children will be set to the value of history, and exit_roots will set to inverse value of history.
+      keep_roots = true,
+      link_roots = true,
+      link_children = true,
+      exit_roots = false,
+
+      delete_check_events = { "TextChanged" },
+      region_check_events = { "CursorMoved", "CursorHold", "InsertEnter" }, -- Leave the current snippet if the cursor is outside it's region
+      update_events = { "TextChanged", "TextChangedI" }, -- Updates snippets as you type
     })
 
     require("luasnip.loaders.from_lua").lazy_load({ paths = "~/.dotfiles/nvim/lua/snippets" })
