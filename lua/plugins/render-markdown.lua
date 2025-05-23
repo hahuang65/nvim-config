6fa3d5be90f417b4f5dcdf3810a2005ebb866f66
Howard Huang <hao@hwrd.me>
2025-05-22 21:29:11 -0500

    Update some render-markdown settings

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -2,16 +2,23 @@
 
 return {
   "MeanderingProgrammer/render-markdown.nvim", -- Make Markdown buffers look beautiful
-  ft = { "markdown", "codecompanion" },
+  ft = { "codecompanion", "markdown", "quarto" },
   opts = {
     render_modes = true, -- Render in ALL modes
     sign = {
       enabled = false, -- Turn off in the status column
     },
     completions = {
+      blink = {
+        enabled = true,
+      },
       lsp = {
         enabled = true,
       },
     },
+    heading = {
+      border = true,
+    },
+    pipe_table = { preset = "heavy" },
   },
 }
